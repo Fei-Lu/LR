ab829cef1f2655dc79fb1a019d429ef1969cb9c6
Fei Lu <dr.lufei@gmail.com>
2019-12-10 01:26:31 +0800

    Tutorial 2

--- src/Tutorial.R
+++ src/Tutorial.R
@@ -1,1 +1,29 @@
-mydata$sum <- mydata$x1 + mydata$x2
+# R annotation
+# Ctrl+Shift+c
+
+#Importing data
+myfile = "/Users/feilu/Documents/RStudioProjects/LR/LR/testData/geneExpression.txt";
+mydata = read.table(myfile, header=TRUE, sep="\t")
+
+# Discriptive statistics
+# get means for variables in data frame mydata
+# excluding missing values
+sapply(mydata, mean, na.rm = TRUE)
+
+#Plotting in R
+attach(mtcars)
+plot(wt, mpg)
+abline(lm(mpg~wt))
+title("Regression of MPG on Weight")
+
+# Packages
+.libPaths()
+library()
+search()
+
+# Getting Help
+help.start()
+help(mean) 
+?mean
+apropos("mean")
+example(mean)
